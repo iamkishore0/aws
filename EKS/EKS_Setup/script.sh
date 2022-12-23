97957f9b6ce259aa184636dc29f99baec8c7a58a
A_Devops_Guy <98376417+iamkishore0@users.noreply.github.com>
2022-12-23 10:40:54 +0530

    Create script.sh

---
+++ EKS/EKS_Setup/script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+export PATH="/usr/local/bin:$PATH"
+echo export PATH="/usr/local/bin:$PATH" >> ~/.bashrc
+
+curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.24.7/2022-10-31/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+export PATH="/usr/local/bin:$PATH"
+echo PATH="/usr/local/bin:$PATH" >> ~/.bashrc
+eksctl version
+kubectl version
