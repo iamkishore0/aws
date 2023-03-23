f2469f6c5d241f272ec446fd0ce85cf4f15f3977
A_Devops_Guy <98376417+iamkishore0@users.noreply.github.com>
2023-03-23 07:51:33 +0530

    Update minikube.sh

--- EKS/EKS_Setup/minikube.sh
+++ EKS/EKS_Setup/minikube.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.24.7/2022-10-31/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+export PATH='/usr/local/bin:$PATH'
+echo export PATH='/usr/local/bin:$PATH' >> ~/.bashrc
